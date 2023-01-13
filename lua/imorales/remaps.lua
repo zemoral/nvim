181350fd8f5fe3b5df043759fb2fbf12f23ffaf0
ZeM10 <isaiah2017@gmail.com>
2023-01-13 06:54:36 -0600

    add: vertical movement, netrw keybindings

--- lua/imorales/remaps.lua
+++ lua/imorales/remaps.lua
@@ -1,12 +1,10 @@
 local map    = require("imorales.keymap")
 local silent = { silent = true }
 
-
 -- inspect token
 map.nnoremap('<leader>tt', function()
-    vim.cmd("TSHighlightCapturesUnderCursor")
+    vim.cmd(":TSHighlightCapturesUnderCursor")
 end, silent)
-
 
 -- line up, down
 map.nnoremap('<A-j>', function()
@@ -17,7 +15,6 @@ map.nnoremap('<A-k>', function()
     vim.cmd(':m .-2')
 end, silent)
 
-
 -- telescope
 map.nnoremap('<C-p>', function()
     require('telescope.builtin').find_files()
@@ -26,3 +23,14 @@ end, silent)
 map.nnoremap('<C-s>', function()
     require('telescope.builtin').live_grep()
 end, silent)
+
+-- netrw
+map.nnoremap('<C-e>', function()
+    vim.cmd(':Ex')
+end, silent)
+
+-- page up / down
+map.nnoremap('{', '<C-U>zz', silent)
+
+map.nnoremap('}', '<C-D>zz', silent)
+
