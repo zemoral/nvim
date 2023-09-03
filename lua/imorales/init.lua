5dc4f3567e05f071962752d44bf9e2ff0d080736
Isaiah Morales <isaiah.r.morales@gmail.com>
2023-09-03 13:30:39 -0500

    fix: leader should be set before plugins register their keybindings

--- lua/imorales/init.lua
+++ lua/imorales/init.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable-next-line: different-requires
 require('imorales.packer')
+require('imorales.option')
 require('imorales.plugin')
 require('imorales.color')
-require('imorales.option')
-require('imorales.keybind')
 require('imorales.autocmd')
+require('imorales.keybind')
