33f98e57834e576ad7662cba837a71eee6712ecb
Isaiah Morales <isaiah.r.morales@gmail.com>
2023-11-13 20:33:07 -0600

    chore: update nvim options

--- lua/imorales/option.lua
+++ lua/imorales/option.lua
@@ -1,24 +1,26 @@
--- globals
+-------------
+-- globals --
+-------------
 vim.g.mapleader = " "
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- options
+-------------
+-- options --
+-------------
 vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
+
+vim.opt.hlsearch = true
+vim.opt.termguicolors = true
 
 vim.opt.errorbells = false
+vim.opt.updatetime = 350
 
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
-vim.opt.swapfile = false
-vim.opt.hlsearch = false
-
-vim.opt.updatetime = 350
 
